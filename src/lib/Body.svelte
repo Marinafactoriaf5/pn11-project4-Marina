799101cb8914295923f66c11e4c686eddd515876
Marina Rull Valdivieso <marinarullvaldivieso@Marinas-MacBook-Pro-26.local>
2023-10-17 15:37:53 +0200

    estilo

---
+++ src/lib/Body.svelte
@@ -0,0 +1,16 @@
+<body>
+<div class="hero">
+    <img src= "src/assets/cover.jpeg" alt="a man in a bed with a woman" class="heroImg">
+</div>
+</body>
+<style>
+    .heroImg{
+        height: 810px;
+        width: 1470px;
+        position:relative;
+        margin-left: -80px;
+        margin-top: 50px;
+        z-index: -5;
+        
+    }
+</style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,91 @@
+<script>
+
+</script>
+
+<header class="menu">
+ <img src= "src/assets/logo.png" alt="logo filmin" class="logoFilmin">
+  
+  <div>
+    <ul class="lista">
+      <li class="lista-item"><a href="enlace1.html">Inici</a></li>
+      <li class="lista-item"><a href="enlace2.html">Pel·lícules</a></li>
+      <li class="lista-item"><a href="enlace2.html">Sèries</a></li>
+      <li class="lista-item"><a href="enlace3.html">Col·leccions</a></li>
+      <li class="lista-item"><a href="enlace2.html">Festivals</a></li>
+      <li class="lista-item"><a href="enlace2.html">Kids</a></li>
+      <li class="lista-item"><a href="enlace2.html">Més</a></li>
+    </ul>
+  </div>
+  <div class="topButtons">
+    <button class="greenButton">
+
+    </button>
+    <button class="transparentButton">
+
+    </button>
+ </div>
+  
+<img src= "src/assets/glass.png" alt="magnifying glass" class="glass">
+</header>
+
+<style>
+ .logoFilmin{
+    height: 33.6px;
+    width: 136;
+    display:flex;
+    margin: 10px;
+
+ }
+
+ .menu{
+    background-image: url('src/assets/rectangle.png');
+    width: 1440px;
+    height: 40px;
+    display:flex;
+    margin-top: px;
+    margin-left: -30px
+
+
+ }
+ 
+ .lista {
+    display: flex;
+    flex-direction: row;
+    list-style: none;
+    padding: 0;
+}
+.lista-item {
+    font-family: Helvetica, Arial, sans-serif;
+    color: white;
+    margin-right: 20px; 
+}
+
+.greenButton{
+    background-color: #02FFA1;
+    width:120px;
+    height:32px;
+    border-radius: 5px;
+    border: none;
+    margin-top: 15px;
+    margin-left: 400px;
+}
+.transparentButton{
+    background-color: transparent;
+    width:120px;
+    height:32px;
+    border-radius: 5px;
+    border: 3;
+    border-color: white;
+    margin-top: 15px;
+    margin-left: 10px;
+}
+
+.topButtons{
+    display: flex;
+
+}
+</style>
+
+<header>
+
+</header>
